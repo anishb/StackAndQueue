e70b6c05c8c35965ce8d3b256d9d44c55236d62a
Anish Basu <abasu1983@gmail.com>
2015-10-08 12:43:30 -0400

    Initial Commit

---
+++ StackAndQueue/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  StackAndQueue
+//
+//  Created by Anish Basu on 10/8/15.
+//  Copyright © 2015 Anish Basu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
